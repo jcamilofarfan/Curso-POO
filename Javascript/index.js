36aea08a5b2bb12b59b100b1e9ea20a245cc850e
jcamilofarfan <farfan1976@hotmail.com>
2022-01-28 16:30:37 -0500

    Aplicando herencia en lenguaje Python y JavaScript

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,3 +1,6 @@
 let car = new Car("ABC12", new Account("Juan Perez", "12345678"));
 car.passenger = 4;
 car.printDataCar()
+let uberX= new UberX("QWE123", new Account("Juan Farfan", "987654321"), "Chevrolet", "Spark");
+uberX.passenger = 4;
+uberX.printDataCar();
