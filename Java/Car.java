1445af89f2980476f998ea7f6f8391da6d987bb4
jcamilofarfan <farfan1976@hotmail.com>
2022-02-01 11:10:11 -0500

    Encapsulamiento

--- Java/Car.java
+++ Java/Car.java
@@ -2,7 +2,7 @@ class Car {
     Integer id;
     String license;
     Account driver;
-    Integer passenger;
+    private Integer passenger;
     
     public Car(String license, Account driver) {
         this.license = license;
@@ -10,6 +10,20 @@ class Car {
     }
 
     void printDataCar(){
-        System.out.println("Car License: " + license + " Name Driver" + driver.name + " Passengers: " + passenger);
+        if(passenger != null){
+            System.out.println("Car License: " + license + " Name Driver" + driver.name + " Passengers: " + passenger);
+        }
+    }
+
+    public Integer getPassenger() {
+        return passenger;
+    }
+
+    public void setPassenger(Integer passenger) {
+        if (passenger == 4) {
+            this.passenger = passenger;
+        } else {
+            System.out.println("The number of passengers must be 4");
+        }
     }
 }
--- Java/Main.java
+++ Java/Main.java
@@ -2,7 +2,7 @@ class Main{
     public static void main(String[] args) {
         System.out.println("Hello World!");
         UberX uberx = new UberX("ABC-123", new Account("Juan", "123.456.789-10"), "Chevrolet", "Spark");
-        uberx.passenger = 3;
+        uberx.setPassenger(7);
         uberx.printDataCar();
 
         // Car car2 = new Car("DEF-456", new Account("Pedro", "987.654.321-09"));
